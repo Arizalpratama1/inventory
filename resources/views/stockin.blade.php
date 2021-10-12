aed8fdb1a5fe370c930472f6c40421ae6f902d89
arizalpratama1 <aizhal.pratama@gmail.com>
2021-10-12 16:27:14 +0700

    commit8

---
+++ resources/views/stockin.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.cuba')
+
+@section('content')
+<div class="page-body">
+  <div class="container-fluid">
+    <div class="page-title">
+      <div class="row">
+        <div class="col-6">
+          <h3>Kelola Stock Barang Masuk</h3>
+        </div>
+        <div class="col-6">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">                                       <i data-feather="home"></i></a></li>
+            <li class="breadcrumb-item active">Kelola Stock Barang Masuk</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Zero Configuration  Starts-->
+        <div class="col-sm-12">
+          <div class="card">
+            <div class="card-header">
+              <h5>Kelola Stock Barang Masuk</h5>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="display" id="basic-1">
+                  <thead>
+                    <tr>
+                      <th>Kode barang</th>
+                      <th>Nama Barang</th>
+                      <th>Stock Sekarang</th>
+                      <th>Stock Masuk</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>63</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        <!-- Zero Configuration  Ends-->
+  </div>
+</div>
+@endsection
